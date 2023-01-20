bdb3ef821a6d15f8f3412c30ccf5202fd863c4d3
Yuliya <iksulia1@gmail.com>
2023-01-20 20:13:34 +0200

    first commit

---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,19 @@
+import img from "./error.gif";
+
+const ErrorMessage = () => {
+  return (
+    <img
+      style={{
+        display: "block",
+        width: "250px",
+        height: "250px",
+        objectFit: "contain",
+        margin: "0 auto",
+      }}
+      src={img}
+      alt="Error"
+    />
+  );
+  //   <img src={process.env.PUBLIC_URL + "/error.gif"} />; // если картинка лежит в папке public
+};
+export default ErrorMessage;
